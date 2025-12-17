480656d2f59484fcaf1f645ca132403e4d3308aa
Ajay Pawar <pawarajay7.ap@gmail.com>
2025-12-17 11:42:19 +0530

    first commit

---
+++ lib/axios-client.ts
@@ -0,0 +1,17 @@
+import axios from "axios"
+import { getAuthToken } from "./api"
+
+const axiosClient = axios.create({
+  baseURL: "http://localhost:5000/api",
+})
+
+axiosClient.interceptors.request.use((config) => {
+  const token = getAuthToken()
+  if (token) {
+    config.headers = config.headers || {}
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+export default axiosClient
